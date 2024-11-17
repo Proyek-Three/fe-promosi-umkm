bdc7a9e998dfe3d682e2d6cd0dd51fe5c64a5561
ghaidafasya24 <ghaidafasya5@gmail.com>
2024-11-17 13:09:31 +0700

    add details product using modals

--- assets/js/products.js
+++ assets/js/products.js
@@ -27,3 +27,19 @@ function filterProducts(selectedCategory) {
   document.addEventListener('DOMContentLoaded', () => {
     filterProducts(document.querySelector('.filter-category.active'));
   });
+
+
+
+  function showModal(button) {
+    const product = button.closest('.product-item');
+    document.getElementById('modalProductName').textContent = product.getAttribute('data-name');
+    document.getElementById('modalStoreName').textContent = product.getAttribute('data-store');
+    document.getElementById('modalPrice').textContent = product.getAttribute('data-price');
+    document.getElementById('modalDescription').textContent = product.getAttribute('data-description');
+    document.getElementById('modalAddress').textContent = product.getAttribute('data-address');
+    document.getElementById('productModal').classList.remove('hidden');
+  }
+
+  function closeModal() {
+    document.getElementById('productModal').classList.add('hidden');
+  }
