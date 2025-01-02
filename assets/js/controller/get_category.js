6dc5aec24f7829851d60b92aa38f3662bc5dce66
ghaidafasya24 <ghaidafasya5@gmail.com>
2025-01-02 19:18:24 +0700

    fetch get category done

--- assets/js/controller/get_category.js
+++ assets/js/controller/get_category.js
@@ -2,13 +2,15 @@ import { addInner } from "https://bukulapak.github.io/element/process.js";
 // import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
 import { isiTabel } from "../temp/table_category.js";
 
-export function GetAllMenu(results) {
-    results.forEach(isiRow);
+export function GetAllCategory(results) {
+
+    const hasil = results.forEach(isiRow);
+    console.log(hasil);
 }
 
 function isiRow(value) {
     let content =
-        isiTabel.replace("#CATEGORYNAME#", value.category.category_name)
+        isiTabel.replace("#CATEGORYNAME#", value.category_name)
             .replace("#IDEDIT#", value._id)
             .replace("#IDDELETE#", value._id)    
     addInner("iniTabel", content);
