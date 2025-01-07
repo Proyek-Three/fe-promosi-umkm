1c25e481f0fe2612e1e777f42f8a5b4d95ae6b69
ghaidafasya24 <ghaidafasya5@gmail.com>
2025-01-07 14:29:49 +0700

    fetch delete product

---
+++ assets/js/controller/delete_product.js
@@ -0,0 +1,23 @@
+function confirmDelete(IDHAPUS) {
+    if (confirm("Apakah ingin menghapus data ID " + IDHAPUS + "?")) {
+        deleteData(IDHAPUS);
+    }
+}
+
+function deleteData(IDHAPUS) {
+    var productId = IDHAPUS;
+    var target_url = "https://bp-promosi-umkm-0fd00e17451e.herokuapp.com/product//delete/" + productId;
+
+    var requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            alert(result.message);
+            location.reload();
+        })
+        .catch(error => console.log('Error:', error));
+}
